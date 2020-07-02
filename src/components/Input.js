de202177c8edd747aeddb67b2e15323c73e7294d
Ilithor <daryk.kohler@gmail.com>
2020-07-01 20:34:32 -0700

    Created Input component

---
+++ src/components/Input.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useMessageData } from "../MessageBoardContext";
+export const Input = () => {
+  const { onSendMessage, text, setText } = useMessageData();
+
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    setText("");
+    onSendMessage(text);
+  };
+  return (
+    <div className="Input">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={text}
+          type="text"
+          placeholder="Enter your message..."
+        />
+        <button>Send</button>
+      </form>
+    </div>
+  );
+};
